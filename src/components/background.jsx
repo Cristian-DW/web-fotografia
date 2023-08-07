8e8b1688e6fbc4693e813e9f841502b2a09f23b1
Cristian-DW <cristtiiank@gmail.com>
2023-08-06 20:02:03 -0500

    update background

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Background = () => {
   return (
-    <div className="background h-full grid grid-cols-20 grid-rows-20 gap-2 p-4 bg-purple-200">
+    <ul className="background h-full grid grid-cols-20 grid-rows-20 gap-2 p-4 bg-purple-200">
       {Array.from({ length: 20 * 20 }).map((_, index) => (
-        <span key={index} ></span>
+        <li key={index} ></li>
       ))}
-    </div>
+    </ul>
   );
 };
 
