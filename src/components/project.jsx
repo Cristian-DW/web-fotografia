92592dd9009764ddfab8fd67f41bc3a396b08699
Cristian-DW <cristtiiank@gmail.com>
2023-08-08 18:37:34 -0500

    create project

---
+++ src/components/project.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+function Projects() {
+    return (
+      <div id="porfolio" className=" porfolio">
+        <div className="creations">
+          <h3>Porfolio</h3>
+          <p>
+            Explora mis creaciones
+          </p>
+        </div>
+        <div className="projects">
+            <article className="project">
+                
+            </article>
+            <article className="project">
+           
+              
+            </article>
+            <article className="project">
+            
+              
+            </article>
+            <article className="project">
+            
+              
+            </article>
+        </div>
+      </div>
+    );
+  }
+
+  export default Projects;
