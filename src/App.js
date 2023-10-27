d658feb293f5bc83c4158ddcf5d3f34f45ba66c0
Cristian-DW <cristtiiank@gmail.com>
2023-10-27 15:18:43 -0500

    project structure change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 
 import './App.css'
-import Background from './components/extras/background';
 import Hero from './components/hero';
 import Nav from './components/nav';
-import Social from './components/social';
 import About from './components/about';
 import Education  from './components/education';
 import Skills from './components/skills';
@@ -18,9 +16,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Social/>
-      <header className="header fixed top-0 left-0 w-screen ">
-      <Background/>
+      <header className="header fixed top-0 left-0 w-screen "> 
       <Hero/>
       </header>
       <main className=' top-main md:relative md:top-[70rem]  lg:top-mainz -20  bg-fondo'>
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Title from "./extras/word";
 import Down from '../media/down.svg'
 
 
@@ -13,7 +12,7 @@ function Hero() {
     <div className=" fixed left-0 top-0  z-10 w-full flex  justify-center h-screen">
       <div className="container mx-auto flex flex-col items-center justify-center  ">
         <h1 className=" title mb-3  animate-fade-down animate-once animate-delay-[900ms] animate-ease-linear  ">Desarrollador Frontend</h1>
-        <Title />
+        
         <button className="button w-48 hover:text-fondo2  animate-fade-down animate-once animate-delay-[1500ms] animate-ease-linear  ">
           <span class="relative z-10"><a href="#porfolio">Explora mis creaciones </a></span>
         </button>
