1bd8c792e24f4dae3f6a08e64d1571c03dc1b99a
Cristian-DW <cristtiiank@gmail.com>
2023-10-31 16:22:40 -0500

    changes in section (about)

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,14 +13,14 @@ function About() {
         <p className="text-md font-light  lg:text-3xl">
         En Frame Fusion, la fotografía es nuestra pasión y vida. Somos un apasionado equipo de fotógrafos comprometidos en capturar momentos únicos y transformarlos en recuerdos eternos. Cada clic de nuestra cámara es un intento de detener el tiempo y capturar la autenticidad de la vida. Nuestra gama de servicios abarca desde bodas emocionales y retratos personales únicos hasta la cobertura de eventos y la inmortalización de impresionantes paisajes. Somos narradores visuales, buscando constantemente la innovación y la creatividad en cada proyecto. En Frame Fusion, la fotografía es una forma de arte y una poderosa forma de comunicación. Permítenos ser la razón detrás de tus recuerdos inolvidables y proyectos visuales excepcionales. Contáctanos y únete a nosotros en este emocionante viaje a través de la magia de la fotografía.
         </p>
-        <button className=" hidden   button  lg:hover:text-fondo mt-20 w-48">
+        <button className=" hidden   button  lg:hover:text-fondo mt-20 w-48 ">
           <span class="relative z-10">
-            <a href="#porfolio">Contáctame</a>
+            <a href="#porfolio">Contáctanos</a>
           </span>
         </button>
       </div>
-      <div className="  md:col-span-3  ">
-        <img  className="w-full" src={imgAbout} alt=""/>
+      <div className="  md:col-span-3   ">
+        <img  className="max-h-screen" src={imgAbout} alt=""/>
         
       </div>
       
