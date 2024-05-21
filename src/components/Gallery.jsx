08846c892cccb09c7e534e0d723b03cf58074f37
CRISTIAN CAMILO CASTRO PINEDA <120737102+Cristian-DW@users.noreply.github.com>
2024-05-20 20:32:10 -0500

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-//images
-import Photo1 from '../media/photos-gallery/photo1.jpg'
-import Photo2 from '../media/photos-gallery/photo2.jpg'
-import Photo3 from '../media/photos-gallery/photo3.jpg'
-import Photo4 from '../media/photos-gallery/photo4.jpg'
-import Photo5 from '../media/photos-gallery/photo5.jpg'
-import Photo6 from '../media/photos-gallery/photo6.jpg'
-import Photo7 from '../media/photos-gallery/photo7.jpg'
-import Photo8 from '../media/photos-gallery/photo8.jpg'
 
+import Photo1 from '../media/photos-gallery/photo1.jpg';
+import Photo2 from '../media/photos-gallery/photo2.jpg';
+import Photo3 from '../media/photos-gallery/photo3.jpg';
+import Photo4 from '../media/photos-gallery/photo4.jpg';
+import Photo5 from '../media/photos-gallery/photo5.jpg';
+import Photo6 from '../media/photos-gallery/photo6.jpg';
+import Photo7 from '../media/photos-gallery/photo7.jpg';
+import Photo8 from '../media/photos-gallery/photo8.jpg';
 
 /**
- * Renders a gallery of photos.
+ * Gallery component renders a gallery of photos in a grid layout.
  *
- * @returns {JSX.Element} The gallery component.
+ * @component
+ * @example
+ * return (
+ *   <Gallery />
+ * )
  */
 export default function Gallery() {
   return (
@@ -30,44 +33,45 @@ export default function Gallery() {
       >
         {/* Photo 1 */}
         <div className="col-span-2 row-span-3 shadow-md shadow-zinc-700 border border-gray-400 overflow-hidden">
-          <img src={Photo1} alt="img" width="auto" height="auto" />
+          <img src={Photo1} alt="Photo 1" width="auto" height="auto" />
         </div>
 
         {/* Photo 2 */}
         <div className="col-span-1 col-start-3 row-span-2 shadow-md shadow-zinc-700 border border-gray-400 overflow-hidden">
-          <img src={Photo2} alt="img" width="100%" height="auto" />
+          <img src={Photo2} alt="Photo 2" width="100%" height="auto" />
         </div>
 
         {/* Photo 3 */}
         <div className="col-span-1 col-start-4 row-span-3 shadow-md shadow-zinc-700 border border-gray-400 overflow-hidden">
-          <img src={Photo3} alt="img" width="100%" height="auto" />
+          <img src={Photo3} alt="Photo 3" width="100%" height="auto" />
         </div>
 
         {/* Photo 4 */}
         <div className="col-span-1 col-start-1 row-span-3 shadow-md shadow-zinc-700 border border-gray-400 overflow-hidden">
-          <img src={Photo4} alt="img" width="100%" height="auto" />
+          <img src={Photo4} alt="Photo 4" width="100%" height="auto" />
         </div>
 
         {/* Photo 5 */}
         <div className="col-span-1 col-start-2 row-span-3 shadow-md shadow-zinc-700 border border-gray-400 overflow-hidden">
-          <img src={Photo5} alt="img" width="100%" height="auto" />
+          <img src={Photo5} alt="Photo 5" width="100%" height="auto" />
         </div>
 
         {/* Photo 6 */}
         <div className="col-span-1 col-start-3 row-span-2 row-start-3 shadow-md shadow-zinc-700 border border-gray-400 overflow-hidden">
-          <img src={Photo6} alt="img" width="100%" height="auto" />
+          <img src={Photo6} alt="Photo 6" width="100%" height="auto" />
         </div>
 
         {/* Photo 7 */}
         <div className="col-span-1 col-start-4 row-span-3 shadow-md shadow-zinc-700 border border-gray-400 overflow-hidden">
-          <img src={Photo7} alt="img" width="100%" height="auto" />
+          <img src={Photo7} alt="Photo 7" width="100%" height="auto" />
         </div>
 
         {/* Photo 8 */}
         <div className="col-span-1 col-start-3 row-span-2 row-start-5 shadow-md shadow-zinc-700 border border-gray-400 overflow-hidden">
-          <img src={Photo8} alt="img" width="100%" height="auto" />
+          <img src={Photo8} alt="Photo 8" width="100%" height="auto" />
         </div>
       </section>
     </>
   );
 }
+
