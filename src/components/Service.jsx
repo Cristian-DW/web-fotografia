5f2c703cd6a5cb02c46cd832a5c2f73bef91bc25
CRISTIAN CAMILO CASTRO PINEDA <120737102+Cristian-DW@users.noreply.github.com>
2024-05-21 20:54:30 -0500

    docs(Services): ✏️ add documentation comments to Services component

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,34 +2,57 @@ import React from "react";
 import Card from "./Card";
 import Img from "../media/logo.png";
 
-
-function Services(){
-   
-
-   return (
-     <>
-       <section id="services" className="w-screen min-h-screen flex flex-col items-center justify-center">
-         <h2 className="text-5xl text-center mb-44 font-Audiowide">Servicios</h2>
-         <div className=" w-screen flex justify-around items-center">
-         <Card imgCard={Img} 
-         titCard='Digitalización'
-          contentCard='Podemos ayudarte en la digitalización de tus fotografias para que perduren en el tiempo'
-           contentButton='Ver Más' 
-           />
-         <Card imgCard={Img} 
-         titCard='Digitalización'
-          contentCard='Podemos ayudarte en la digitalización de tus fotografias para que perduren en el tiempo'
-           contentButton='Ver Más' 
-           />
-         <Card imgCard={Img} 
-         titCard='Digitalización' 
-         contentCard='Podemos ayudarte en la digitalización de tus fotografias para que perduren en el tiempo'
-          contentButton='Ver Más'
-           />
-         </div>
-       </section>
-     </>
-   );
+/**
+ * Componente Services
+ * 
+ * Este componente renderiza una sección que muestra los servicios ofrecidos por la empresa.
+ * Cada servicio se presenta en un componente `Card` que incluye una imagen, un título,
+ * una descripción y un botón.
+ * 
+ * @component
+ * @example
+ * return (
+ *   <Services />
+ * )
+ */
+function Services() {
+  return (
+    <>
+      {/* Sección de servicios con estilo flex para centrar el contenido */}
+      <section id="services" className="w-screen min-h-screen flex flex-col items-center justify-center">
+        {/* Título de la sección */}
+        <h2 className="text-5xl text-center mb-44 font-Audiowide">Servicios</h2>
+        
+        {/* Contenedor de las tarjetas de servicios */}
+        <div className="w-screen flex justify-around items-center">
+          
+          {/* Tarjeta de servicio: Digitalización */}
+          <Card 
+            imgCard={Img}
+            titCard='Digitalización'
+            contentCard='Podemos ayudarte en la digitalización de tus fotografías para que perduren en el tiempo'
+            contentButton='Ver Más'
+          />
+          
+          {/* Tarjeta de servicio: Digitalización */}
+          <Card 
+            imgCard={Img}
+            titCard='Digitalización'
+            contentCard='Podemos ayudarte en la digitalización de tus fotografías para que perduren en el tiempo'
+            contentButton='Ver Más'
+          />
+          
+          {/* Tarjeta de servicio: Digitalización */}
+          <Card 
+            imgCard={Img}
+            titCard='Digitalización'
+            contentCard='Podemos ayudarte en la digitalización de tus fotografías para que perduren en el tiempo'
+            contentButton='Ver Más'
+          />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Services;
